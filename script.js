311319eab56275bd1d10385098b6c2d407a0bf14
manish mandal <76776194mm@gmail.com>
2022-09-03 16:22:06 +0530

    update script.js

--- script.js
+++ script.js
@@ -23,14 +23,12 @@ const closeModal = function () {
 btnsOpenModal.forEach(function(btn){
   btn.addEventListener('click',openModal);
 })
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) { 
     closeModal();
   }
 });
