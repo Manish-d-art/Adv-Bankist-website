170d7854b0adb4dcfd8d554ad3698ee0bc9c3cb8
manish mandal <76776194mm@gmail.com>
2022-09-05 21:23:55 +0530

    update script.js

--- script.js
+++ script.js
@@ -118,3 +118,19 @@ window.addEventListener('scroll',function(){
 });
 
 
+//intersection observer
+const obsCallback=function(entries,observer){
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+
+const obsOptions={
+  root:null,
+  // threshold:0.1,
+  threshold:[0,0.2],
+}
+
+const observer=new IntersectionObserver(obsCallback,obsOptions);
+observer.observe(section1);
+
